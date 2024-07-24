fa0bceedd8a4495e764366bad9fa483b5231a369
yaoyuhang <118644871+wcjzx@users.noreply.github.com>
2024-07-24 10:48:36 +0800

    学习《第一行代码》实践MVVM框架开发天气预报软件

---
+++ app/src/main/java/com/example/fristlinecode_weatherforcast/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.fristlinecode_weatherforcast
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.fristlinecode_weatherforcast.ui.theme.FristLineCode_WeatherForcastTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FristLineCode_WeatherForcastTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FristLineCode_WeatherForcastTheme {
+        Greeting("Android")
+    }
+}
